e50eeb70f5bacd3def6c07cb1cc5f09849883309
youssef houmas <92693238+ysfzxz7@users.noreply.github.com>
2025-07-20 12:31:54 +0100

    the hero componants

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Hero from "./componants/Hero";
 import Nav from "./componants/Nav";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="">
       <div className="bg-primary ">
         <Nav />
+        <Hero />
       </div>
     </div>
   );
---
+++ src/componants/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <div className="w-fit mx-auto text-center p-20">
+      <h1 className="text-5xl font-bold text-white">Become a React DEV</h1>
+      <p className="text-white ">
+        Find the React job that fits your skills and needs
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
