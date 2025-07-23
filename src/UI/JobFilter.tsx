c0103c222f149a97c3efc98e8742a50fda445948
youssef houmas <92693238+ysfzxz7@users.noreply.github.com>
2025-07-23 21:38:18 +0100

    style : fix responsivness issue

--- src/UI/JobFilter.tsx
+++ src/UI/JobFilter.tsx
@@ -3,7 +3,7 @@ import TypeFilter from "./TypeFilter";
 
 const JobFilter = () => {
   return (
-    <div className="flex rounded justify-between border-gray-300 border-1  bg-amber-50 mb-10 p-2">
+    <div className="lg:flex space-y-3 mx-2 rounded justify-between border-gray-300 border-1  bg-amber-50 mb-10 p-2">
       <div className="flex gap-2">
         <TypeFilter />
         <LocationFilter />
